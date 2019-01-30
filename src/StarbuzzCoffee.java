a5bc5a32bb1e2242a40e2f41803b830369cefd66
alexmickelson <alexmickelson96@gmail.com>
2019-01-30 11:53:51 -0700

    added gitignore

--- src/StarbuzzCoffee.java
+++ src/StarbuzzCoffee.java
@@ -1,23 +1,11 @@
 public class StarbuzzCoffee {
     public static void main (String args[]){
         Beverage beverage = new Espresso();
-        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+
+        beverage.print();
 
 
 
-        Beverage beverage2 = new HouseBlend();
-        beverage2.print();
-        beverage2.frame();
 
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Mocha(beverage2);
-        beverage2 = new Mocha(beverage2);
-        beverage2.print();
-
-
-        beverage.frame();
-        beverage2.frame();
     }
 }
